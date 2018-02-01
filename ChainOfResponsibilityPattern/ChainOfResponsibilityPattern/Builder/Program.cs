2b0b8561fa4ee072b11c027fee83967b50f3a536
Karthik Konkula <kkonkula1192@gmail.com>
2018-02-02 01:39:46 +0530

    FluentInterface pattern added

---
+++ ChainOfResponsibilityPattern/ChainOfResponsibilityPattern/Builder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityPattern.Builder
+{
+    // builder pattern is just a way to build an object similar to what a constructor does
+    // Reference https://stackoverflow.com/questions/328496/when-would-you-use-the-builder-pattern
+    public class Program
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}
---
+++ ChainOfResponsibilityPattern/ChainOfResponsibilityPattern/FluentInterface/IContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityPattern.FluentInterface
+{
+    public interface IContext
+    {
+        ICustomer FirstName(string name);
+        ICustomer LastName(string name);
+        ICustomer Age(int age);
+        string FirstName();
+        string LastName();
+        int Age();
+    }
+}
