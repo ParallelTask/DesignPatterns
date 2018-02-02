944b40e5caec2939190abd05227e15a85940fd98
Karthik Kumar Konkula <kkonkula1192@gmail.com>
2018-02-02 14:13:21 +0530

    Added Builer Pattern

---
+++ CharacterBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityPattern.Builder
+{
+    public class CharacterBuilder : ICharacterBuilder
+    {
+        private bool _hasShoes { get; set; }
+        private string _hairColor { get; set; }
+        private string _shirtColor { get; set; }
+        private bool _hasGun { get; set; }
+        private bool _hasArrow { get; set; }
+
+        public void AddArrow()
+        {
+            _hasArrow = true;
+        }
+
+        public void RemoveArrow()
+        {
+            _hasArrow = false;
+        }
+
+        public void AddGun()
+        {
+            _hasGun = true;
+        }
+
+        public void RemoveGun()
+        {
+            _hasGun = false;
+        }
+
+        public void AddShoes()
+        {
+            _hasShoes = true;
+        }
+
+        public void RemoveShoes()
+        {
+            _hasShoes = false;
+        }
+
+        public void ChangeHairColor(string color)
+        {
+            _hairColor = color;
+        }
+
+        public void ChangeShirt(string color)
+        {
+            _shirtColor = color;
+        }
+
+        public ICharacter Build()
+        {
+            return new Character() { HairColor = _hairColor, HasArrow = _hasArrow, HasGun = _hasGun, HasShoes = _hasShoes, ShirtColor = _shirtColor };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Run the observable pattern
+            // Observable.Program.Run();
+
+            // Run the chain of responsibility pattern
+            // ChainOfResponsibility.Program.Run();
+
+            // Run the publish subscribe pattern
+            // PublishSubscribe.Program.Run();
+
+            // Run the selected observables pattern
+            // SelectedObservable.Program.Run();
+
+            // Run the fluent interface pattern
+            // FluentInterface.Program.Run();
+
+            // Run the builder pattern
+            // Builder.Program.Run();
+
+            Console.ReadLine();
+        }
+    }
+}
